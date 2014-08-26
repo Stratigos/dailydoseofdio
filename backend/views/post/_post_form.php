b64321beb2f0b26ffaf146d922f3b4f4b5ddd136
Todd <Stratigos@users.noreply.github.com>
2014-08-26 16:16:16 -0400

    removing irrelevant/redundant comment

--- backend/views/post/_post_form.php
+++ backend/views/post/_post_form.php
@@ -99,8 +99,6 @@
         <!--
             TODO: include posts.type_id switch here, and include a new partial form
                 depending on the Post type selected (img, video, quote, etc...)
-              - then include list of Tag instances for now, and crate ctrl routine
-                to add PostTags to Post on save
         -->
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']); ?>
     <?php ActiveForm::end(); ?>
