e9430cb7b878f304ea5c5d7cdedcdd240ef35ea7
Todd <Stratigos@users.noreply.github.com>
2014-08-28 10:14:36 -0400

    Yii2 Framework Updates; TWBS Updates;

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf28a844f56901d9596b22d641fa9903c::getLoader();
+return ComposerAutoloaderInit1715e88e0781fc7db2a5fe8e43ed8bf1::getLoader();
