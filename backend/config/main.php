233a55e75c0fb9077274e25b61a1478753117618
Todd <Stratigos@users.noreply.github.com>
2014-10-08 15:40:54 -0400

    starting to implement backend routes (got em for Post)

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,28 +7,26 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
-    'basePath' => dirname(__DIR__),
+    'id'                  => 'app-backend',
+    'basePath'            => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [],
-    'components' => [
+    'bootstrap'           => ['log'],
+    'modules'             => [],
+    'components'          => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'ihavenoideawhatiamdoing',
         ],
         'urlManager' => [
             'enablePrettyUrl'     => TRUE,
-            //'enableStrictParsing' => TRUE,
+            //'enableStrictParsing' => TRUE, // if TRUE, will only allow routes defined in 'rules'
             'showScriptName'      => FALSE,
             'class'               => 'yii\web\UrlManager',
-            // 'rules'               => [
-            //     // [
-            //     //     'class'      => 'yii\rest\UrlRule',
-            //     //     'controller' => 'user'
-            //     // ]
-            //     //'site/index' => 'index.php?r=site/index'
-            // ]
+            'rules'               => [
+                'posts'                       => 'post/index',
+                'post/update/<id:\d+>'        => 'post/update',
+                'post/create/<media_type:\d>' => 'post/create'
+            ]
         ],
         'user' => [
             'identityClass' => 'common\models\User',
