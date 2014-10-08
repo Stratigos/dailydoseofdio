e4bc3374a4f91d57209160974620c63d9a22c72e
Todd <Stratigos@users.noreply.github.com>
2014-10-08 15:46:17 -0400

    adding TODO note about using aliases

--- common/config/aliases.php
+++ common/config/aliases.php
@@ -3,3 +3,7 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+/**
+ * @todo remove const PROJECT_WEB_DIR, and create a backend/config/aliases.php
+ *  file which will produce an alias for @uploads to @backend/web/uploads 
+ */
