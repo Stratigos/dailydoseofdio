b155556dd6d5c8a8ff038182895835b9c2fd51cb
Stratigos <Stratigos@users.noreply.github.com>
2014-11-04 16:43:15 -0500

    playing around with text on homepage

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -81,7 +81,7 @@
 
             <!-- Blog Search Well -->
             <div class="well">
-                <h4>Blog Search</h4>
+                <h4>Search for a Dose</h4>
                 <div class="input-group">
                     <input type="text" class="form-control">
                     <span class="input-group-btn">
@@ -95,7 +95,7 @@
 
             <!-- Blog Categories Well -->
             <div class="well">
-                <h4>Blog Categories</h4>
+                <h4>Doses by Category</h4>
                 <div class="row">
                     <div class="col-lg-6">
                         <ul class="list-unstyled">
@@ -129,8 +129,14 @@
 
             <!-- Side Widget Well -->
             <div class="well">
-                <h4>Side Widget Well</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                <h4>Some Widget Text to Read</h4>
+                <p>
+                    Shoulder in doner, rump ribeye swine labore kevin landjaeger pork frankfurter fatback dolor veniam
+                    prosciutto. Dolore anim magna aute. Chuck jerky brisket pastrami beef excepteur frankfurter.
+                    Pastrami sunt ball tip non pariatur shankle. Ut ham prosciutto pork belly lorem in pig. Eu officia
+                    nulla, pastrami landjaeger jowl ground round chuck spare ribs minim adipisicing ut andouille\
+                    tri-tip. Fatback pork belly meatball laborum andouille.
+                </p>
             </div>
 
         </div>
