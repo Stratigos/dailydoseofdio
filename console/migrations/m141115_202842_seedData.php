5415dc1cec769d036cd8b698c594c493a6f733fa
Stratigos <Stratigos@users.noreply.github.com>
2014-11-15 15:29:32 -0500

    new migration file

---
+++ console/migrations/m141115_202842_seedData.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141115_202842_seedData extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m141115_202842_seedData cannot be reverted.\n";
+
+        return false;
+    }
+}
