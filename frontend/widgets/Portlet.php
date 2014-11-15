23dccf4a5c47af271f146801a47764a4d92d7786
Stratigos <Stratigos@users.noreply.github.com>
2014-11-15 16:25:57 -0500

    Updated Portlet

--- frontend/widgets/Portlet.php
+++ frontend/widgets/Portlet.php
@@ -2,10 +2,11 @@
 /*****************************************************************************
 * Portlet - Sidebar Widgets - by Yeung Song
 * @author netyum@163.com
-* @see https://github.com/netyum/blog2.0/blob/master/app/widgets/Portlet.php
+* @see http://git.oschina.net/netyum/blog2/raw/master/app/widgets/Portlet.php
 * @todo get accompanying CSS from
-*	https://github.com/netyum/blog2.0/blob/master/www/css/site.css
+*      https://github.com/netyum/blog2.0/blob/master/www/css/site.css
 ******************************************************************************/
+
 namespace app\widgets;
 
 use \yii\helpers\Html;
@@ -28,97 +29,91 @@ use \yii\base\Widget;
  */
 class Portlet extends Widget
 {
-	/**
-	 * @var string the tag name for the portlet container tag. Defaults to 'div'.
-	 */
-	public $tagName = 'div';
+    /**
+     * @var string the tag name for the portlet container tag. Defaults to 'div'.
+     */
+    public $tagName='div';
+    /**
+     * @var array the HTML attributes for the portlet container tag.
+     */
+    public $htmlOptions=array('class'=>'portlet');
+    /**
+     * @var string the title of the portlet. Defaults to null.
+     * When this is not set, Decoration will not be displayed.
+     * Note that the title will not be HTML-encoded when rendering.
+     */
+    public $title;
+    /**
+     * @var string the CSS class for the decoration container tag. Defaults to 'portlet-decoration'.
+     */
+    public $decorationCssClass='';
+    /**
+     * @var string the CSS class for the portlet title tag. Defaults to 'portlet-title'.
+     */
+    public $titleCssClass='portlet-title';
+    /**
+     * @var string the CSS class for the content container tag. Defaults to 'portlet-content'.
+     */
+    public $contentCssClass='portlet-body';
+    /**
+     * @var boolean whether to hide the portlet when the body content is empty. Defaults to true.
+     * @since 1.1.4
+     */
+    public $hideOnEmpty=true;
 
-	/**
-	 * @var array the HTML attributes for the portlet container tag.
-	 */
-	public $htmlOptions = array('class' => 'portlet');
+    private $_beginTag;
 
-	/**
-	 * @var string the title of the portlet. Defaults to null.
-	 * When this is not set, Decoration will not be displayed.
-	 * Note that the title will not be HTML-encoded when rendering.
-	 */
-	public $title;
+    /**
+     * Initializes the widget.
+     * This renders the open tags needed by the portlet.
+     * It also renders the decoration, if any.
+     */
+    public function init()
+    {
+        ob_start();
+        ob_implicit_flush(false);
 
-	/**
-	 * @var string the CSS class for the decoration container tag. Defaults to 'portlet-decoration'.
-	 */
-	public $decorationCssClass = '';
+        $this->htmlOptions['id']=$this->getId();
+        echo Html::beginTag($this->tagName,$this->htmlOptions)."\n";
+        $this->renderDecoration();
+        echo "<div class=\"{$this->contentCssClass}\">\n";
 
-	/**
-	 * @var string the CSS class for the portlet title tag. Defaults to 'portlet-title'.
-	 */
-	public $titleCssClass = 'portlet-title';
+        $this->_beginTag=ob_get_contents();
+        ob_clean();
+    }
 
-	/**
-	 * @var string the CSS class for the content container tag. Defaults to 'portlet-content'.
-	 */
-	public $contentCssClass = 'portlet-body';
+    /**
+     * Renders the content of the portlet.
+     */
+    public function run()
+    {
+        $this->renderContent();
+        $content=ob_get_clean();
+        if($this->hideOnEmpty && trim($content)==='')
+            return;
+        echo $this->_beginTag;
+        echo $content;
+        echo '</div>';
+        echo Html::endTag($this->tagName);
+    }
 
-	/**
-	 * @var boolean whether to hide the portlet when the body content is empty. Defaults to true.
-	 * @since 1.1.4
-	 */
-	public $hideOnEmpty = true;
-
-	private $_beginTag;
-
-	/**
-	 * Initializes the widget.
-	 * This renders the open tags needed by the portlet.
-	 * It also renders the decoration, if any.
-	 */
-	public function init()
-	{
-		ob_start();
-		ob_implicit_flush(false);
-
-		$this->htmlOptions['id']=$this->getId();
-		echo Html::beginTag($this->tagName,$this->htmlOptions)."\n";
-		$this->renderDecoration();
-		echo "<div class=\"{$this->contentCssClass}\">\n";
-
-		$this->_beginTag=ob_get_contents();
-		ob_clean();
-	}
-
-	/**
-	 * Renders the content of the portlet.
-	 */
-	public function run()
-	{
-		$this->renderContent();
-		$content=ob_get_clean();
-		if($this->hideOnEmpty && trim($content)==='')
-			return;
-		echo $this->_beginTag;
-		echo $content;
-		echo '</div>';
-		echo Html::endTag($this->tagName);
-	}
-
-	/**
-	 * Renders the decoration for the portlet.
-	 * The default implementation will render the title if it is set.
-	 */
-	protected function renderDecoration()
-	{
-		if($this->title!==null)
-		{
+    /**
+     * Renders the decoration for the portlet.
+     * The default implementation will render the title if it is set.
+     */
+    protected function renderDecoration()
+    {
+        if($this->title!==null)
+        {
                         echo "<h4 class=\"{$this->titleCssClass}\">{$this->title}</h4>\n";
-		}
-	}
+        }
+    }
 
-	/**
-	 * Renders the content of the portlet.
-	 * Child classes should override this method to render the actual content.
-	 */
-	protected function renderContent()
-	{
-	}
+    /**
+     * Renders the content of the portlet.
+     * Child classes should override this method to render the actual content.
+     */
+    protected function renderContent()
+    {
+    }
 }
