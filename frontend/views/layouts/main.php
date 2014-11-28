75fc7975cfda0f1e9a244829facda5e843f243e8
Stratigos <Stratigos@users.noreply.github.com>
2014-11-28 07:39:43 -0500

    removing unused navlinks from frontend layout; enforcing some stylistic pedantry;

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -7,72 +7,60 @@ use frontend\assets\AppAsset;
 use frontend\widgets\Alert;
 
 /* @var $this \yii\web\View */
-/* @var $content string */
 if(empty($this->title)) {
     $this->title = Yii::$app->params['defaultTitle'];
 }
 
 AppAsset::register($this);
 ?>
-<?php $this->beginPage() ?>
+<?php $this->beginPage(); ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language; ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta charset="<?= Yii::$app->charset; ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <?= Html::csrfMetaTags(); ?>
+    <title><?= Html::encode($this->title); ?></title>
+    <?php $this->head(); ?>
 </head>
 <body>
-    <?php $this->beginBody() ?>
+    <?php $this->beginBody(); ?>
     <div class="wrap">
         <?php
             NavBar::begin([
                 'brandLabel' => 'Daily Dose of Dio',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
+                'brandUrl'   => Yii::$app->homeUrl,
+                'options'    => [
+                    'class' => 'navbar-inverse navbar-fixed-top'
+                ]
             ]);
             $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
+                ['label' => 'About',   'url' => ['/site/about']],
+                ['label' => 'Contact', 'url' => ['/site/contact']]
             ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
+                'items'   => $menuItems
             ]);
             NavBar::end();
         ?>
 
         <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []
+            ]); ?>
+            <?= Alert::widget(); ?>
+            <?= $content; ?>
         </div>
     </div>
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; Dudeman Somebody <?= date('Y') ?></p>
+            <p class="pull-left">&copy; Dudeman Somebody 2014</p>
         </div>
     </footer>
 
-    <?php $this->endBody() ?>
+    <?php $this->endBody(); ?>
 </body>
 </html>
-<?php $this->endPage() ?>
+<?php $this->endPage(); ?>
