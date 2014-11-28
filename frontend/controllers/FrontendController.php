74e5688cc40c6323f013b7e226c0e15775142bd7
Stratigos <Stratigos@users.noreply.github.com>
2014-11-28 16:08:09 -0500

    moving functionality to parent from ctrls

--- frontend/controllers/FrontendController.php
+++ frontend/controllers/FrontendController.php
@@ -11,5 +11,19 @@ use yii\web\Controller;
 
 class FrontendController extends Controller
 {
-
+	/**
+	 * @inheritdoc
+	 */
+	public function actions()
+	{
+	    return [
+	        'error' => [
+	            'class' => 'yii\web\ErrorAction',
+	        ],
+	        'captcha' => [
+	            'class'           => 'yii\captcha\CaptchaAction',
+	            'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : NULL,
+	        ]
+	    ];
+	}
 }
--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -9,21 +9,6 @@ use frontend\models\ContactForm;
 
 class SiteController extends FrontendController
 {
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
 
     /**
      * DOING THINGS WITH COMPUTERS
