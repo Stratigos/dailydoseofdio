31fa9ff7f2151b77297a700e480e46c09bb62698
Stratigos <Stratigos@users.noreply.github.com>
2014-11-29 10:32:04 -0500

    adds scopes to CategoryQuery

--- common/models/CategoryQuery.php
+++ common/models/CategoryQuery.php
@@ -1,2 +1,31 @@
 <?php
-// TODO EVERYTHING
+/*********************************
+* Scopes for class Category
+**********************************/
+namespace common\models;
+
+use yii\db\ActiveQuery;
+
+class CategoryQuery extends ActiveQuery
+{
+    /**
+     * Base criteria for a Category to appear in user facing content.
+     * @return self
+     */
+    public function published()
+    {
+        $this->andWhere(['deleted_at' => 0]);
+
+        return $this;
+    }
+
+    /**
+     * Adds descending sort by name to published() criteria
+     * @return self
+     */
+    public function publishedDesc()
+    {
+        $this->published()->addOrderBy(['name' => SORT_DESC]);
+        return $this;
+    }
+}
