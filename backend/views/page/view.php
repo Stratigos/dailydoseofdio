097461a995f5d1e2c340188045be26576a85393d
Stratigos <Stratigos@users.noreply.github.com>
2014-12-22 09:16:04 -0500

    fixes broken page view - needed to add file

---
+++ backend/views/page/view.php
@@ -0,0 +1,16 @@
+<div class="page-view">
+    <div>
+        <h3>View a Page (<?= $page->id ?>)</h3>
+    </div>
+    <div class="body-content">
+        <p>
+            This view is under construction!
+            <pre>
+                <?php echo(print_r($page)); ?>
+            </pre>
+        </p>
+        <p>
+            &#10144;&nbsp;<a href="<?= $indexUrl ?>">Return to Page Index</a>
+        </p>
+    </div>
+</div>
