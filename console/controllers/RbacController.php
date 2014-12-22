7719e8b359a14a314b648dd35ed97045718555f9
Stratigos <Stratigos@users.noreply.github.com>
2014-12-22 09:08:11 -0500

    refined RBAC stuff - removed permissions now handled by Controller::behaviors() and roles

--- console/controllers/RbacController.php
+++ console/controllers/RbacController.php
@@ -31,80 +31,6 @@ class RbacController extends Controller
             $auth->add($authorRule);
 
             // PERMISSIONS ////////////////////////////////////////////////////
-
-            // Blogs
-            $createBlog              = $auth->createPermission('createBlog');
-            $createBlog->description = 'Create Blog';
-            $auth->add($createBlog);
-
-            $updateBlog              = $auth->createPermission('updateBlog');
-            $updateBlog->description = 'Update Blog';
-            $auth->add($updateBlog);
-
-            $viewBlog              = $auth->createPermission('viewBlog');
-            $viewBlog->description = 'View Blog';
-            $auth->add($viewBlog);
-
-            $deleteBlog              = $auth->createPermission('deleteBlog');
-            $deleteBlog->description = 'Delete Blog';
-            $auth->add($deleteBlog);
-
-            // Bloggers
-            $createBlogger              = $auth->createPermission('createBlogger');
-            $createBlogger->description = 'Create Blogger';
-            $auth->add($createBlogger);
-
-            $updateBlogger              = $auth->createPermission('updateBlogger');
-            $updateBlogger->description = 'Update Blogger';
-            $auth->add($updateBlogger);
-
-            $viewBlogger              = $auth->createPermission('viewBlogger');
-            $viewBlogger->description = 'View Blogger';
-            $auth->add($viewBlogger);
-
-            $deleteBlogger              = $auth->createPermission('deleteBlogger');
-            $deleteBlogger->description = 'Delete Blogger';
-            $auth->add($deleteBlogger);
-
-            // Categories
-            $createCategory              = $auth->createPermission('createCategory');
-            $createCategory->description = 'Create Category';
-            $auth->add($createCategory);
-
-            $updateCategory              = $auth->createPermission('updateCategory');
-            $updateCategory->description = 'Update Category';
-            $auth->add($updateCategory);
-
-            $viewCategory              = $auth->createPermission('viewCategory');
-            $viewCategory->description = 'View Category';
-            $auth->add($viewCategory);
-
-            $deleteCategory              = $auth->createPermission('deleteCategory');
-            $deleteCategory->description = 'Delete Category';
-            $auth->add($deleteCategory);
-
-            // Tags
-            $createTag              = $auth->createPermission('createTag');
-            $createTag->description = 'Create Tag';
-            $auth->add($createTag);
-
-            $updateTag              = $auth->createPermission('updateTag');
-            $updateTag->description = 'Update Tag';
-            $auth->add($updateTag);
-
-            $viewTag              = $auth->createPermission('viewTag');
-            $viewTag->description = 'View Tag';
-            $auth->add($viewTag);
-
-            $deleteTag              = $auth->createPermission('deleteTag');
-            $deleteTag->description = 'Delete Tag';
-            $auth->add($deleteTag);
-
-            // Posts
-            $createPost              = $auth->createPermission('createPost');
-            $createPost->description = 'Create Post';
-            $auth->add($createPost);
-
             $updatePost              = $auth->createPermission('updatePost');
             $updatePost->description = 'Update Post';
             $auth->add($updatePost);
@@ -114,97 +40,20 @@ class RbacController extends Controller
             $updateOwnPost->ruleName    = $authorRule->name;
             $auth->add($updateOwnPost);
 
-            $viewPost              = $auth->createPermission('viewPost');
-            $viewPost->description = 'View Post';
-            $auth->add($viewPost);
-
-            $deletePost              = $auth->createPermission('deletePost');
-            $deletePost->description = 'Delete Post';
-            $auth->add($deletePost);
-
-            // Pages
-            $createPage              = $auth->createPermission('createPage');
-            $createPage->description = 'Create Page';
-            $auth->add($createPage);
-
-            $updatePage              = $auth->createPermission('updatePage');
-            $updatePage->description = 'Update Page';
-            $auth->add($updatePage);
-
-            $viewPage              = $auth->createPermission('viewPage');
-            $viewPage->description = 'View Page';
-            $auth->add($viewPage);
-
-            $deletePage              = $auth->createPermission('deletePage');
-            $deletePage->description = 'Delete Page';
-            $auth->add($deletePage);
-
-            // Dio Sites
-            $createDioSite              = $auth->createPermission('createDioSite');
-            $createDioSite->description = 'Create DioSite';
-            $auth->add($createDioSite);
-
-            $updateDioSite              = $auth->createPermission('updateDioSite');
-            $updateDioSite->description = 'Update DioSite';
-            $auth->add($updateDioSite);
-
-            $viewDioSite              = $auth->createPermission('viewDioSite');
-            $viewDioSite->description = 'View DioSite';
-            $auth->add($viewDioSite);
-
-            $deleteDioSite              = $auth->createPermission('deleteDioSite');
-            $deleteDioSite->description = 'Delete DioSite';
-            $auth->add($deleteDioSite);
-
-            // ROLES //////////////////////////////////////////////////////////
-
             // add "updateOwnPost" to "updatePost" Permission
             $auth->addChild($updateOwnPost, $updatePost);
 
-            // creating "author" role and giving permission for almost everything 
-            //  except modifying Pages and taxonomic records
+            // ROLES //////////////////////////////////////////////////////////
             $author = $auth->createRole('author');
             $auth->add($author);
-            $auth->addChild($author, $createPost);
-            $auth->addChild($author, $viewPost);
             $auth->addChild($author, $updateOwnPost);
-            $auth->addChild($author, $createDioSite);
-            $auth->addChild($author, $updateDioSite);
-            $auth->addChild($author, $viewDioSite);
-            $auth->addChild($author, $deleteDioSite);
-            $auth->addChild($author, $viewPage);
-            $auth->addChild($author, $viewBlog);
-            $auth->addChild($author, $viewBlogger);
-            $auth->addChild($author, $viewCategory);
-            $auth->addChild($author, $createTag);
-            $auth->addChild($author, $viewTag);
 
-            // creating "admin" role which does everything "author" does
-            //  and also everything else :)
             $admin = $auth->createRole('admin');
             $auth->add($admin);
             $auth->addChild($admin, $author);
             $auth->addChild($admin, $updatePost);
-            $auth->addChild($admin, $deletePost);
-            $auth->addChild($admin, $createPage);
-            $auth->addChild($admin, $updatePage);
-            $auth->addChild($admin, $deletePage);
-            $auth->addChild($admin, $createBlog);
-            $auth->addChild($admin, $updateBlog);
-            $auth->addChild($admin, $deleteBlog);
-            $auth->addChild($admin, $createBlogger);
-            $auth->addChild($admin, $updateBlogger);
-            $auth->addChild($admin, $deleteBlogger);
-            $auth->addChild($admin, $createCategory);
-            $auth->addChild($admin, $updateCategory);
-            $auth->addChild($admin, $deleteCategory);
-            $auth->addChild($admin, $updateTag);
-            $auth->addChild($admin, $deleteTag);
 
             // ASSIGNMENTS
-
-            // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
-            // usually implemented in your User model.
             $auth->assign($author, $userLucy->id);
             $auth->assign($admin, $userTodd->id);
 
