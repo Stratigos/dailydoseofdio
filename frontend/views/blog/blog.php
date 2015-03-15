c3e6af7630b5ce6d955552f2dad420ea39a33a58
Stratigos <Stratigos@users.noreply.github.com>
2015-03-15 10:46:19 -0400

    consistency with Blog index and other indices

--- frontend/views/blog/blog.php
+++ frontend/views/blog/blog.php
@@ -7,14 +7,12 @@
             <?= Html::img($blog->getImage('200x200')); ?>
             <p><b>Description:</b>&nbsp;<?= $blog->description; ?> </p>
         </div>
-        <div class="container">
-            <?= ListView::widget([
-                'dataProvider' => $postsDP,
-                'itemView'     => '@frontend/views/_partials/postDefault.php',
-                'emptyText'    => 'No Doses found.',
-                'separator'    => Html::tag('hr'),
-                'summary'      => 'Doses of Dio found in this Blog'
-            ]); ?>
-        </div>
+        <?= ListView::widget([
+            'dataProvider' => $postsDP,
+            'itemView'     => '@frontend/views/_partials/postDefault.php',
+            'emptyText'    => "No Doses found for {$blog->title}.",
+            'separator'    => Html::tag('hr'),
+            'summary'      => 'Doses of Dio found in this Blog'
+        ]); ?>
     </div>
 </div>
