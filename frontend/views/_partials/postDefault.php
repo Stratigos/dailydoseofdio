69c072a30fa03faa3fd9aec27714c20d88083c26
Stratigos <Stratigos@users.noreply.github.com>
2015-03-15 10:37:52 -0400

    refactor of Post partial view

--- frontend/views/_partials/postDefault.php
+++ frontend/views/_partials/postDefault.php
@@ -1,17 +1,22 @@
 <?php use yii\helpers\Html; ?>
-<p class="lead"><?= Html::a(Html::encode($model->title), $model->url); ?></p>
+<p class="lead"><a href="<?= $model->url ?>"><?= Html::encode($model->title) ?></a></p>
 <p>by <a href="index.php"><?= $model->blogger? $model->blogger->name : 'Nobody Special'; ?></a></p>
 <p><span class="glyphicon glyphicon-time"></span> <?= date('Y-m-d H:i:s', $model->published_at); ?></p>
 <hr />
 <div class="row">
-    <div class="col-md-4 col-sm-4">
+    <div class="col-md-5 col-sm-5">
         <?php if($model->image_path) : ?>
             <?= Html::img($model->getImage('250x155'), ['title' => "{$model->title} Thumbnail"]); ?>
         <?php else: ?>
             <img class="img-responsive" src="http://placehold.it/250x155" alt="">
         <?php endif; ?>
     </div>
-    <div class="col-md-8 col-sm-8"><?= $model->summary; ?><p></div>
+    <p><?= $model->summary; ?></p>
 </div>
-<div class="clearfix"></div>
-<a class="btn btn-primary media" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+<br />
+<div class="control-group">
+    <a
+        class="btn btn-primary"
+        href="<?= $model->url ?>"
+    >Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
