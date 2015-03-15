21cb82eea3f13c3819a515c7e1fd81cf593513ff
Stratigos <Stratigos@users.noreply.github.com>
2015-03-15 10:55:48 -0400

    removing hard coded search box from homepage

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -33,31 +33,6 @@
         <!-- Blog Sidebar Widgets Column -->
         <div class="col-md-4">
             <?= DuckDuckGoSearchPortlet::widget() ?>
-            <div class="well">
-                <h4>Search for a Dose</h4>
-                <form method="get" target="_blank" id="search" action="http://duckduckgo.com/">
-                    <div class="input-group">
-                        <input type="hidden" name="ka" value="h"/>
-                        <input type="hidden" name="kn" value="1"/>
-                        <input type="hidden" name="kt" value="Helvetica"/>
-                        <input type="hidden" name="kae" value="d"/>
-                        <input type="hidden" name="sites" value="dailydoseofdio.com"/>
-                        <input
-                            class="form-control"
-                            type="text"
-                            name="q"
-                            maxlength="255"
-                            placeholder="Powered by DuckDuckGo"
-                        />
-                        <!-- <input type="submit" value="DuckDuckGo Search" style="visibility: hidden;" /> -->
-                        <span class="input-group-btn">
-                            <button id="ddgsearchbtn" class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                            </button>
-                        </span>
-                    </div>
-                </form> 
-            </div>
 
             <!-- Blog Categories Well -->
             <div class="well">
