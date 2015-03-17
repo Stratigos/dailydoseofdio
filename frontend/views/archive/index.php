eda2cc85527999ea8e68c03fc0f8cc81c7c2373e
Stratigos <Stratigos@users.noreply.github.com>
2015-03-17 17:19:20 -0400

    updating archives index to use consistent sidebar

--- frontend/views/archive/index.php
+++ frontend/views/archive/index.php
@@ -3,14 +3,12 @@
     <div class="row">
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
             <h1 class="page-header">Dose Archives</h1>
-
             <?php if(isset($posts) && !empty($posts)) :?>
                 <?php foreach($posts as $post) : ?>
                     <?= $this->context->renderPartial(
                         '@frontend/views/_partials/postDefault.php',
-                        ['post' => $post]
+                        ['model' => $post]
                     ); ?>
                 <?php endforeach; ?>
                 <?= LinkPager::widget(['pagination' => $pagination]); ?>
@@ -18,76 +16,10 @@
                 <p>No Posts found.</p>
             <?php endif;?>
         </div>
-
         <!-- Blog Sidebar Widgets Column -->
         <div class="col-md-4">
-
-            <!-- Blog Search Well -->
-            <div class="well">
-                <h4>Search for a Dose</h4>
-                <div class="input-group">
-                    <input type="text" class="form-control">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">
-                            <span class="glyphicon glyphicon-search"></span>
-                    </button>
-                    </span>
-                </div>
-                <!-- /.input-group -->
-            </div>
-
-            <!-- Blog Categories Well -->
-            <div class="well">
-                <h4>Doses by Category</h4>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- /.col-lg-6 -->
-                    <div class="col-lg-6">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                            <li><a href="#">Category Name</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- /.col-lg-6 -->
-                </div>
-                <!-- /.row -->
-            </div>
-
-            <!-- Side Widget Well -->
-            <div class="well">
-                <h4>Some Widget Text to Read</h4>
-                <p>
-                    Shoulder in doner, rump ribeye swine labore kevin landjaeger pork frankfurter fatback dolor veniam
-                    prosciutto. Dolore anim magna aute. Chuck jerky brisket pastrami beef excepteur frankfurter.
-                    Pastrami sunt ball tip non pariatur shankle. Ut ham prosciutto pork belly lorem in pig. Eu officia
-                    nulla, pastrami landjaeger jowl ground round chuck spare ribs minim adipisicing ut andouille\
-                    tri-tip. Fatback pork belly meatball laborum andouille.
-                </p>
-            </div>
-
+            <?= $this->context->renderPartial('@frontend/views/_partials/sidebar.php'); ?>
         </div>
-
     </div>
-    <!-- /.row -->
-
     <hr>
-
 </div>
-<!-- /.container -->
