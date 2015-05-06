1ce4618a4c3a7fc2a15fbf1aebd0f4a0e1215811
Stratigos <Stratigos@users.noreply.github.com>
2015-05-06 10:05:48 -0400

    added containers to blog index partial to better align images and text

--- frontend/views/_partials/blogDefault.php
+++ frontend/views/_partials/blogDefault.php
@@ -1,13 +1,17 @@
 <?php use yii\helpers\Html; ?>
-<h2><?= Html::a(Html::encode($model->title), $model->url); ?></h2>
-<div class="row">
-    <?= Html::img($model->getImage('200x200')); ?>
+<div class="container">
+    <h2><?= Html::a(Html::encode($model->title), $model->url); ?></h2>
+    <div class="row">
+      <?= Html::img($model->getImage('200x200')); ?>
+    </div>
+    <div class="row">
+      <p><?= $model->short_description ?></p>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row">
+        <a
+          class="btn btn-primary media"
+          href="<?= Yii::$app->urlManager->createUrl($model->url) ?>"
+        >Read Blog <span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
 </div>
-<div class="row">
-    <p><?= $model->short_description ?></p>
-</div>
-<div class="clearfix"></div>
-<a
-    class="btn btn-primary media"
-    href="<?= Yii::$app->urlManager->createUrl($model->url) ?>"
->Read Blog <span class="glyphicon glyphicon-chevron-right"></span></a>
